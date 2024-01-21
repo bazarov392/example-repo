2c37d277d89d38fb9e352ad51df64096d0cb58c9
Maut <vadimbudrik@gmail.com>
2024-01-21 23:22:07 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { Color, EventsSDK, Hero, LocalPlayer, RendererSDK, Vector2 } from "github.com/octarine-public/wrapper/index"
 
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
 })
-
 EventsSDK.on('Draw', () => {
-	console.log('Draw', new Date());
+	RendererSDK.FilledRect(new Vector2(200, 200), new Vector2(200, 200), Color.Orange);
 });
