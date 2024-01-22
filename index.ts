ddf81e0b0523c883f42e5645ddffc5cda6f4100a
Maut <vadimbudrik@gmail.com>
2024-01-22 15:22:44 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, item_flask } from "github.com/octarine-public/wrapper/index"
 
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
 })
 
 EventsSDK.on("UnitItemsChanged", e => {
-	console.log(e)
+	const inv = e.Inventory
+	const flasks = inv.TotalItems.filter(i => i !== undefined && i !== null && i instanceof item_flask)
+	console.log("flasks", flasks)
 })
