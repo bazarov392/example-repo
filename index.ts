df0c326e9518b29f5561d9549c0d750fd55ec3c3
Maut <vadimbudrik@gmail.com>
2024-01-22 16:25:12 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ EventsSDK.on("Draw", () => {
 	const oneProcentHP = hero.MaxHP / 100
 
 	if (oneProcentHP * 20 >= hero.HP) {
-		UseFlaskSleeper.Sleep(140000)
+		UseFlaskSleeper.Sleep(14000)
 		console.log("use flask", new Date())
 	}
 })
