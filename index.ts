b0002b7685b3c5b8ba96f59634298f7b9279f736
Maut <vadimbudrik@gmail.com>
2024-01-22 14:45:04 +0300

    asd

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-import { Color, EventsSDK, Hero, LocalPlayer, RendererSDK, Vector2 } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, InputEventSDK } from "github.com/octarine-public/wrapper/index"
 
 EventsSDK.on("GameStarted", () => {
 	console.log("Hello world!")
+})
+
+InputEventSDK.on("KeyUp", key => {
+	console.log(key)
 })
