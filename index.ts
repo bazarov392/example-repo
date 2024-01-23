ae727136b985f31740cec6aa824adfb332845d01
Maut <vadimbudrik@gmail.com>
2024-01-23 18:09:38 +0300

    asd

--- index.ts
+++ index.ts
@@ -1,62 +1,31 @@
-import { EventsSDK, item_flask, LocalPlayer, TickSleeper } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, item_flask, LocalPlayer } from "github.com/octarine-public/wrapper/index"
 
-class AutoUseFlask {
-	public flask: Nullable<item_flask>
-	public freeSlot: Nullable<number>
-	public isBackpack: boolean = false
-}
-const AUseFlask = new AutoUseFlask()
-const [UseFlaskSleeper] = [new TickSleeper()]
+let itemFlask: Nullable<item_flask>
 
-EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
+EventsSDK.on("UnitItemsChanged", ent => {
+	if (!ent.IsMyHero) {
+		return
+	}
+
+	const flask = ent.GetItemByClass(item_flask)
+	if (flask === undefined) {
+		itemFlask = undefined
+		return
+	}
+	return (itemFlask = flask)
 })
 
-EventsSDK.on("UnitItemsChanged", e => {
-	const inv = e.Inventory
-	let flask: Nullable<item_flask>
-	console.log("total items", inv.TotalItems)
-	if (AUseFlask.freeSlot !== undefined && inv.TotalItems[AUseFlask.freeSlot] !== undefined) {
-		AUseFlask.freeSlot = undefined
-	}
-	inv.TotalItems.map((item, index) => {
-		if (item === undefined) {
-			if (AUseFlask.freeSlot === undefined && index < 5) {
-				AUseFlask.freeSlot = index
-			}
-		} else if (item instanceof item_flask) {
-			flask = item
-			AUseFlask.isBackpack = item.ItemSlot >= 6 && item.ItemSlot <= 8
-		}
-	})
-
-	// const flask = inv.TotalItems.find(i => i instanceof item_flask)
-	AUseFlask.flask = flask
-
-	// console.log("flask", AUseFlask)
-})
-
-EventsSDK.on("Tick", () => {
-	if (UseFlaskSleeper.Sleeping) {
+EventsSDK.on("PostDataUpdate", () => {
+	const localHero = LocalPlayer?.Hero
+	if (!localHero || !itemFlask) {
 		return false
 	}
-	const hero = LocalPlayer?.Hero
-	if (!hero) {
-		return false
-	}
-	const oneProcentHP = hero.MaxHP / 100
-
-	if (oneProcentHP * 20 >= hero.HP) {
-		// UseFlaskSleeper.Sleep(14000)
-		if (AUseFlask.flask !== undefined) {
-			if (AUseFlask.isBackpack && AUseFlask.freeSlot !== undefined) {
-				hero.MoveItem(AUseFlask.flask, AUseFlask.freeSlot)
-				UseFlaskSleeper.Sleep(3500)
-			} else {
-				UseFlaskSleeper.Sleep(14000)
-				hero.CastTarget(AUseFlask.flask, hero)
-				// if(AUseFlask.flask)
-			}
+	const onePercentHP = localHero.MaxHP / 100
+	const thresholdHP = onePercentHP * 50
+	if (thresholdHP >= localHero.HP) {
+		if (!itemFlask.CanBeCasted() || localHero.HasBuffByName("modifier_flask_healing")) {
+			return false
 		}
+		localHero.CastTarget(itemFlask, localHero)
 	}
 })
