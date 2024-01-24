b1e850bede4440cc14517a03a5607cbbfe04fc9e
Maut <vadimbudrik@gmail.com>
2024-01-24 22:54:25 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ EventsSDK.on("PostDataUpdate", () => {
 	const onePercentHP = localHero.MaxHP / 100
 	const thresholdHP = onePercentHP * 50
 	if (thresholdHP >= localHero.HP) {
-		if (onAttack && itemFlask.CanBeCasted() && !localHero.HasBuffByName("modifier_flask_healing")) {
+		if (!onAttack && itemFlask.CanBeCasted() && !localHero.HasBuffByName("modifier_flask_healing")) {
 			return localHero.CastTarget(itemFlask, localHero)
 		}
 	}
