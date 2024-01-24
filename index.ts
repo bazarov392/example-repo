d4415692928572915ceeef3b5cb546073141d884
Maut <vadimbudrik@gmail.com>
2024-01-24 23:23:09 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -33,7 +33,10 @@ EventsSDK.on("Tick", () => {
 	let c = true
 	console.log(enemyHeroes)
 	for (const hero of enemyHeroes) {
-		c = hero.Distance2D(localHero) > hero.BaseAttackRange
+		if (hero.Distance2D(localHero) > hero.BaseAttackRange) {
+			c = false
+			break
+		}
 	}
 	onAttack = c
 })
