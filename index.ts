71397f575a7e31530c073efb776e9c83e0b52f74
Maut <vadimbudrik@gmail.com>
2024-01-25 20:08:14 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, ImageData, RendererSDK, Vector2 } from "github.com/octarine-public/wrapper/index"
 
-EventsSDK.on("LinearProjectileCreated", tile => console.log("LinearProjectileCreated", tile))
+EventsSDK.on("GameStarted", () => {
+	console.log()
+})
+// EventsSDK.on("LinearProjectileCreated", tile => console.log("LinearProjectileCreated", tile))
 
-EventsSDK.on("TrackingProjectileCreated", tile => console.log("TrackingProjectileCreated", tile))
+// EventsSDK.on("TrackingProjectileCreated", tile => console.log("TrackingProjectileCreated", tile))
 
-EventsSDK.on("EntityCreated", ent => console.log("EntityCreated", ent))
+// EventsSDK.on("EntityCreated", ent => console.log("EntityCreated", ent))
 
-EventsSDK.on("FakeUnitCreated", ent => console.log("FakeUnitCreated", ent))
+// EventsSDK.on("FakeUnitCreated", ent => console.log("FakeUnitCreated", ent))
 
-EventsSDK.on("AbilityCooldownChanged", a => console.log("AbilityCooldownChanged", a))
+// EventsSDK.on("AbilityCooldownChanged", a => {
+// 	const hero = LocalPlayer?.Hero
+// 	if (!hero) {
+// 		return
+// 	}
+// 	console.log("A", a.Distance2D(hero))
+// })
+
+EventsSDK.on("Draw", () => {
+	RendererSDK.Image(ImageData.Paths.Icons.tower_dire, new Vector2(-1447, -656))
+})
