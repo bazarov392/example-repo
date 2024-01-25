934146407b016734a6e1f12396924d8c858f70c9
Maut <vadimbudrik@gmail.com>
2024-01-26 02:05:01 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,26 +3,28 @@ import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 EventsSDK.on("GameStarted", () => {
 	console.log()
 })
-EventsSDK.on("LinearProjectileCreated", tile => console.log("LinearProjectileCreated", tile))
+// EventsSDK.on("LinearProjectileCreated", tile => console.log("LinearProjectileCreated", tile))
 
 EventsSDK.on("TrackingProjectileCreated", tile => console.log("TrackingProjectileCreated", tile))
 
-EventsSDK.on("EntityCreated", ent => console.log("EntityCreated", ent))
+EventsSDK.on("TrackingProjectileUpdated", tile => console.log("TrackingProjectileUpdated", tile))
 
-EventsSDK.on("FakeUnitCreated", ent => console.log("FakeUnitCreated", ent))
+// EventsSDK.on("EntityCreated", ent => console.log("EntityCreated", ent))
+
+// EventsSDK.on("FakeUnitCreated", ent => console.log("FakeUnitCreated", ent))
 
 EventsSDK.on("EntityVisibleChanged", ent => console.log("EntityVisibleChanged", ent))
 
-EventsSDK.on("AbilityCooldownChanged", abil => {
-	const owner = abil.OwnerEntity
-	if (!owner) {
-		return
-	}
+// EventsSDK.on("AbilityCooldownChanged", abil => {
+// 	const owner = abil.OwnerEntity
+// 	if (!owner) {
+// 		return
+// 	}
 
-	if (owner.IsValid && owner.IsEnemy()) {
-		console.log("AbilityCooldownChanged", abil)
-	}
-})
+// 	if (owner.IsValid && owner.IsEnemy()) {
+// 		console.log("AbilityCooldownChanged", abil)
+// 	}
+// })
 
 // EventsSDK.on("AbilityCooldownChanged", a => {
 // 	const hero = LocalPlayer?.Hero
