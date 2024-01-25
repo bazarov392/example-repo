cf496e80039cc0af6367af195def0186bb5436f8
Maut <vadimbudrik@gmail.com>
2024-01-25 16:38:26 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 
-EventsSDK.on("LinearProjectileCreated", tile => {
-	console.log(new Date(), tile)
-})
+EventsSDK.on("LinearProjectileCreated", tile => console.log("LinearProjectileCreated", tile))
+
+EventsSDK.on("TrackingProjectileCreated", tile => console.log("TrackingProjectileCreated", tile))
