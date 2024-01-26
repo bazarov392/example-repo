46ff3a0ecda5b856f25608a9db6a73fff0e81c6c
Maut <vadimbudrik@gmail.com>
2024-01-26 04:25:11 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,39 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, Unit } from "github.com/octarine-public/wrapper/index"
+
+const enemyHeroesInfo: {
+	[key: string]: {
+		tpCooldown: number
+		lastPosition: {
+			x: number
+			y: number
+		}
+		visible: boolean
+	}
+} = {}
 
 EventsSDK.on("GameStarted", () => {
 	console.log()
 })
 // EventsSDK.on("LinearProjectileCreated", tile => console.log("LinearProjectileCreated", tile))
 
-EventsSDK.on("TrackingProjectileCreated", tile => console.log("TrackingProjectileCreated", tile))
+// EventsSDK.on("TrackingProjectileCreated", tile => console.log("TrackingProjectileCreated", tile))
 
-EventsSDK.on("TrackingProjectileUpdated", tile => console.log("TrackingProjectileUpdated", tile))
+// EventsSDK.on("TrackingProjectileUpdated", tile => console.log("TrackingProjectileUpdated", tile))
 
-EventsSDK.on("EntityCreated", ent => console.log("EntityCreated", ent))
+EventsSDK.on("EntityCreated", ent => {
+	if (ent instanceof Unit && ent.IsHero && ent.IsEnemy()) {
+		if (!(ent.Name_ in enemyHeroesInfo)) {
+			enemyHeroesInfo[ent.Name_] = {
+				tpCooldown: 0,
+				lastPosition: {
+					x: ent.Position.x,
+					y: ent.Position.y
+				},
+				visible: ent.IsVisible
+			}
+		}
+	}
+})
 EventsSDK.on("PreEntityCreated", ent => console.log("PreEntityCreated", ent))
 
 EventsSDK.on("EntityVisibleChanged", ent => console.log("EntityVisibleChanged", ent))
@@ -17,16 +41,18 @@ EventsSDK.on("EntityVisibleChanged", ent => console.log("EntityVisibleChanged", 
 EventsSDK.on("UnitAnimation", npc => console.log("UnitAnimation", npc))
 EventsSDK.on("UnitAnimationEnd", npc => console.log("UnitAnimationEnd", npc))
 
-// EventsSDK.on("AbilityCooldownChanged", abil => {
-// 	const owner = abil.OwnerEntity
-// 	if (!owner) {
-// 		return
-// 	}
+EventsSDK.on("AbilityCooldownChanged", abil => {
+	const hero = abil.OwnerEntity
+	if (!hero) {
+		return
+	}
 
-// 	if (owner.IsValid && owner.IsEnemy()) {
-// 		console.log("AbilityCooldownChanged", abil)
-// 	}
-// })
+	if (!(hero instanceof Unit) || !hero.IsHero || !hero.IsEnemy() || !(hero.Name_ in enemyHeroesInfo)) {
+		return
+	}
+
+	console.log("abil", abil)
+})
 
 // EventsSDK.on("AbilityCooldownChanged", a => {
 // 	const hero = LocalPlayer?.Hero
