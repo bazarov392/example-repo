a5ffc787eba5fc918203937b08e5bfc19317c705
Maut <vadimbudrik@gmail.com>
2024-01-26 06:28:30 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Color, EventsSDK, RendererSDK, Unit, Vector2 } from "github.com/octarine-public/wrapper/index"
+import { Color, EventsSDK, RendererSDK, Unit, Vector2, Vector3 } from "github.com/octarine-public/wrapper/index"
 import { Paths } from "github.com/octarine-public/wrapper/wrapper/Data/ImageData"
 
 const PathParticleTeleport = "particles/items2_fx/teleport_end.vpcf"
@@ -12,13 +12,17 @@ EventsSDK.on("GameStarted", () => {
 // EventsSDK.on("TrackingProjectileUpdated", tile => console.log("TrackingProjectileUpdated", tile))
 
 EventsSDK.on("Draw", () => {
+	const w2sPosition = RendererSDK.WorldToScreen(new Vector3(-1194, -861, 128))
+	if (!w2sPosition) {
+		return
+	}
 	// const w2sPosition = RendererSDK.WorldToScreen(new Vector2(-1194, -861))
 	// if (!w2sPosition) {
 	// 	return
 	// }
 	// const position = w2sPosition.Subtract(new Vector2(-1194, -861).DivideScalar(2))
 
-	RendererSDK.Image(Paths.Icons.icon_svg_creep, new Vector2(200, 200), -1, new Vector2(20, 20), Color.White)
+	RendererSDK.Image(Paths.Icons.icon_svg_creep, w2sPosition, -1, new Vector2(20, 20), Color.White)
 })
 
 EventsSDK.on("ParticleDestroyed", particle => {
