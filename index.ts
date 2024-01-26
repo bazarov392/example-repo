da058a79c9d51c4f0ac9b22fc2e8b7523a853c06
Maut <vadimbudrik@gmail.com>
2024-01-26 06:12:41 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { EventsSDK, Unit } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK, RendererSDK, Unit, Vector2 } from "github.com/octarine-public/wrapper/index"
+import { Paths } from "github.com/octarine-public/wrapper/wrapper/Data/ImageData"
 
 const PathParticleTeleport = "particles/items2_fx/teleport_end.vpcf"
 
@@ -11,12 +12,16 @@ EventsSDK.on("GameStarted", () => {
 
 // EventsSDK.on("TrackingProjectileUpdated", tile => console.log("TrackingProjectileUpdated", tile))
 
+EventsSDK.on("Draw", () => {
+	RendererSDK.Image(Paths.Icons.icon_svg_creep, new Vector2(-1194, -861))
+})
+
 EventsSDK.on("ParticleDestroyed", particle => {
 	if (particle.Path !== PathParticleTeleport) {
 		return
 	}
 	const ent = particle.ModifiersAttachedTo
 	if (ent instanceof Unit && ent.IsHero && ent.IsEnemy() && particle.ControlPoints.has(0)) {
-		console.log(`${ent.Name_} телепортируется`, particle.ControlPoints.get(0))
+		console.log(`${ent.Name_} телепортировался`, particle.ControlPoints.get(0))
 	}
 })
